96dffdd4fe467d63c39bf95bd3fbd76254211af4
parayaticblue <piteraraya@gmail.com>
2022-03-23 10:21:37 -0300

    Primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,49 @@
+import './mylabel.css';
+
+// se exporta la interfaz para resolver unos inconvenientes a futuro
+export interface Props {
+  /**
+  * Este es el mensaje que se va mostrar en la etiqueta
+  */
+  label: string;
+  /**
+  * Este es el tamaño que tendrá la etiqueta | default: 'normal'
+  */
+  size?: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * Permite capitalizar a mayusuculas todos los caracteres de la etiqueta | default: false 
+   */
+  allCaps?: boolean;
+  /**
+    * Permite cambiar el color a la etiqueta | default: text-primary | text-secondary | text-tertiary
+  */
+  color?: 'text-primary' | 'text-secondary' | 'text-tertiary';
+  /**
+   * Puedes elegir el tipo de color de fondo que quieres para la etiqueta 
+   */
+  backgroundColor?: string;
+  /**
+   * Puedes elegir el tipo de color que quieres para la etiqueta 
+   */
+  fontColor?: string;
+}
+
+export const MyLabel = ({
+  label   = 'No label',
+  size    = 'normal',
+  allCaps = false,
+  color = 'text-primary',
+  backgroundColor,
+  fontColor,
+  ...props
+}: Props) => {
+  return (
+    <span
+      className={`label  ${size} ${color}`}
+      style={{background: backgroundColor, color: fontColor}}
+      {...props}
+    >
+      {allCaps ? label.toUpperCase() : label}
+    </span>
+  )
+}
---
+++ src/stories/components/MyLabel.stories.tsx
@@ -0,0 +1,56 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { MyLabel } from "../../components/MyLabel";
+
+
+// 1. Pide una exportación por defecto
+export default {
+  title: 'UI/Etiquetas/MyLabel', 
+  component: MyLabel,
+  argTypes: {
+    size:{ control: 'select'},
+    color: { control: 'select' },
+    backgroundColor: { control: 'color' },
+    fontColor: { control: 'color' },
+  }
+} as ComponentMeta<typeof MyLabel>;
+
+// 2. Utilización de nuestro componente - definimos el tipo Props a los Label
+const Template: ComponentStory<typeof MyLabel> = (args) => <MyLabel {...args} />;
+
+// 3. Creamos una página : creando una copia rompiendo la referencia que trae javascript por defecto | Para evitar mutaciones
+export const Basic = Template.bind({});
+Basic.args = {
+  label: 'Etiqueta ejemplo',
+  size: 'normal',
+  allCaps: false // true para que capitalize todos los caracteres
+};
+
+export const AllCaps = Template.bind({});
+AllCaps.args = {
+  label: 'Etiqueta all caps',
+  size: 'normal',
+  allCaps: true // true para que capitalize todos los caracteres
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Etiqueta Secondary',
+  size: 'normal',
+  color: 'text-secondary'
+  
+}
+
+
+export const Tertiary = Template.bind({});
+Tertiary.args = {
+  label: 'Etiqueta Tertiary',
+  size: 'normal',
+  color: 'text-tertiary'
+}
+
+export const CustomFontColor = Template.bind({});
+CustomFontColor.args = {
+  size: 'h1',
+  fontColor: '#5517ac',
+};
+
